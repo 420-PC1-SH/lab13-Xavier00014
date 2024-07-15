865fce6405b78f7991f1d70c721fb37b8c31baa4
Xavier00014 <131310111+Xavier00014@users.noreply.github.com>
2024-07-15 10:16:34 -0400

    Exercice 2 - requis 2 pass

--- greeting.js
+++ greeting.js
@@ -1,4 +1,7 @@
 function greet(transmission) {
+    if(transmission === undefined || transmission === null || transmission === ""){
+        return "Hello, there"
+    }
     return "Hello, " + transmission
 }
 module.exports = greet;
