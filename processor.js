82ce8e3234192451c3c9058a7681d7433e1baa7a
Xavier00014 <131310111+Xavier00014@users.noreply.github.com>
2024-07-15 09:22:35 -0400

    Exercice 1 - requis 1 pass

--- processor.js
+++ processor.js
@@ -4,6 +4,12 @@ function processor(transmission) {
     }
     let parts = transmission.split("::");
     let rawData = parts[1];
+    if(rawData[0] !== '<'){
+        throw new Error('Data is invalid ; should contain "<"');
+    }
+    if(rawData[rawData.length-1] !== '>'){
+        throw new Error('Data is invalid ; should contain ">"');
+    }
     return {
         id: Number(parts[0]),
         rawData: rawData
