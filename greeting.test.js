c12abfe062794ff19be3a08c8357098f6fd3649f
Xavier00014 <131310111+Xavier00014@users.noreply.github.com>
2024-07-15 10:13:03 -0400

    Exercice 2 - requis 2 fail

--- greeting.test.js
+++ greeting.test.js
@@ -5,4 +5,16 @@ describe("greeting", function () {
         let result = greet("Luke");
         expect(result).toEqual("Hello, Luke");
     });
+    test("Test greet undefined", function () {
+        let result = greet(undefined);
+        expect(result).toEqual("Hello, there");
+    });
+    test("Test greet null", function () {
+        let result = greet(null);
+        expect(result).toEqual("Hello, there");
+    });
+    test("Test greet ''", function () {
+        let result = greet("");
+        expect(result).toEqual("Hello, there");
+    });
 })
