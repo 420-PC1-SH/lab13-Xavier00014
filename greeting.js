4312296d26fb885e0547187d6a9c00cbbb4ea370
Xavier00014 <131310111+Xavier00014@users.noreply.github.com>
2024-07-15 10:33:08 -0400

    Exercice 2 - requis 4 pass

--- greeting.js
+++ greeting.js
@@ -2,9 +2,17 @@ function greet(name) {
     if(name === undefined || name === null || name === ""){
         return "Hello, there"
     }
+    if(Array.isArray(name)){
+        return greetArray(name);
+    }
     if(name === name.toUpperCase()){
         return "HELLO, " + name
     }
     return "Hello, " + name
 }
+
+function greetArray(name){
+    return "Hello, " + name[0] + " and " + name[1]
+}
+
 module.exports = greet;
